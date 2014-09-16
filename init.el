75eddcc6affc3d2a401f7e89a955303bbe3faf29
Ross Cairns <ross@electricglen.com>
2014-09-16 11:09:29 +0100

    Fix for temporary file saving

--- init.el
+++ init.el
@@ -330,10 +330,8 @@
 
 ;; # Backups
 ;; Put the in the tmp dir
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+(setq backup-directory-alist `((".*" . "~/.emacs.saves")))
+(setq auto-save-file-name-transforms `((".*" "~/.emacs.saves" t)))
 
 ;; # Auto complete
 (require 'auto-complete)
